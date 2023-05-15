f772129a4ac683fcb92467ae1ef671c48125420b
Gloria <libertadglo@gmail.com>
2023-05-15 18:21:06 -0300

    cargada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MicuidadComponent } from './micuidad/micuidad.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorComponent } from './error/error.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,6 +28,7 @@ import { ErrorComponent } from './error/error.component';
     MicuidadComponent,
     DashboardComponent,
     ErrorComponent,
+    
 
   ],
   imports: [
